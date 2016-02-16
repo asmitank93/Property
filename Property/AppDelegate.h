d0a169d83b51a416776de133dea50ba23193a81b
Tops <tops@Topss-iMac.local>
2016-02-16 09:47:37 +0530

    Initial Commit

---
+++ Property/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Property
+//
+//  Created by Tops on 2/16/16.
+//  Copyright (c) 2016 Tops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
